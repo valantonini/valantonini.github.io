dfa601238d97f8928be3cb1aaadb4bdcebc3a7a5
val <val.antonini@gmail.com>
2019-03-29 22:22:52 +1000

    removed old site

--- public/main.js
+++
@@ -1,2 +0,0 @@
-function whiteoutCodeBackgroundOnHover(){$.when($.ready).then("pre code").each(function(){var n=$(this).parent("pre");n.hover(function(){$(this).css("background-color","#ffffff")},function(){$(this).css("background-color","transparent")})})}$(function(){hljs.initHighlightingOnLoad()});
-//# sourceMappingURL=maps/main.js.map
