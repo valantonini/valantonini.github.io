4bdf34a63fcd370a69cff0bc542c6fe59a9f4c29
Travis CI User <travis@example.org>
2019-04-01 10:07:27 +0000

    removing old

--- scripts/search.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var sjs = SimpleJekyllSearch({
-        searchInput: document.getElementById('searchInput'),
-        resultsContainer: document.getElementById('resultsContainer'),
-        json: '/search.json'
-    });
-
-    document.getElementById('searchInput').focus();
-});
